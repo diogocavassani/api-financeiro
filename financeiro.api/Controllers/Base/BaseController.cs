ef56813c4e45b49bcd25429fc2d04d76cf1e84b5
Diogo <diogocavassani@gmail.com>
2023-12-14 21:47:46 -0300

    Adicionar arquivos de projeto.

---
+++ financeiro.api/Controllers/Base/BaseController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace financeiro.api.Controllers.Base
+{
+    public class BaseController : ControllerBase
+    {
+    }
+}
---
+++ financeiro.api/Controllers/ContasPagarController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace financeiro.api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ContasPagarController : ControllerBase
+    {
+
+        [HttpGet("")]
+        public async Task<IActionResult> BuscarContasPagar()
+        {
+
+            return Ok("");
+        }
+    }
+}
