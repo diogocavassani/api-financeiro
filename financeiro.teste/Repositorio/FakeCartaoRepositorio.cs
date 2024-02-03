65253dfea568a06beb8ee40e54dec4b86ed8ef0a
Diogo <diogocavassani@gmail.com>
2024-02-03 16:08:41 -0300

    Implementando Fake repositorios com bogus

---
+++ financeiro.teste/Repositorio/FakeCartaoRepositorio.cs
@@ -0,0 +1,34 @@
+﻿using Bogus;
+using financeiro.dominio.Entidades;
+using financeiro.dominio.Interfaces.Repositorios;
+using financeiro.dominio.ViewModels;
+
+namespace financeiro.infra.Repositorio
+{
+    public class FakeCartaoRepositorio : ICartaoRepositorio
+    {
+        private readonly IList<Cartao> _cartoes;
+
+        public FakeCartaoRepositorio()
+        {
+            var cartaoId = 0;
+            _cartoes = new Faker<Cartao>("pt_BR").StrictMode(false)
+                .RuleFor(p => p.IdCartao, f => f.IndexFaker)
+                .RuleFor(p => p.DiaVencimentoFatura, f => f.Random.Number(1, 30))
+                .RuleFor(p => p.NomeCartao, f => f.Finance.AccountName())
+                .RuleFor(p => p.FlExcluido, f => f.Random.Bool()).Generate(100);
+        }
+
+        public async Task AdicionarAsync(Cartao cartao)
+        {
+            
+        }
+
+        public async Task<List<CartaoResultViewModel>> ObterCartoesAsync() => _cartoes
+            .Where(p => p.FlExcluido == false)
+            .Select(p => new CartaoResultViewModel(p.IdCartao, p.NomeCartao, p.DiaVencimentoFatura)).ToList();
+        public async Task<Cartao?> BuscarPorIdAsync(int idCartao) => _cartoes.Where(c => c.IdCartao == idCartao && c.FlExcluido == false).FirstOrDefault();
+
+
+    }
+}
---
+++ financeiro.teste/Repositorio/FakeContaPagarRepositorio.cs
@@ -0,0 +1,51 @@
+﻿using Bogus;
+using financeiro.dominio.Entidades;
+using financeiro.dominio.Interfaces.Repositorios;
+using financeiro.dominio.ViewModels;
+
+namespace financeiro.infra.Repositorio
+{
+    public class FakeContaPagarRepositorio : IContaPagarRepositorio
+    {
+        private readonly IList<ContaPagar> _contasPagar;
+
+        public FakeContaPagarRepositorio()
+        {
+            _contasPagar = new Faker<ContaPagar>("pt-BR").StrictMode(false)
+                .RuleFor(p => p.IdContaPagar, f => f.IndexFaker)
+                .RuleFor(p => p.ParcelaAtual, f => f.Random.Number())
+                .RuleFor(p => p.TotalParcela, f => f.Random.Number())
+                .RuleFor(p => p.Descricao, f => f.Random.Words(2))
+                .RuleFor(p => p.Valor, f => f.Finance.Amount())
+                .RuleFor(p => p.DataLancamento, f => f.Date.Recent())
+                .RuleFor(p => p.DataVencimento, f => f.Date.Future())
+                .RuleFor(p => p.FlCancelado, f => f.Random.Bool())
+                .RuleFor(p => p.IdCartao, f => f.Random.Int())
+                .Generate(100);
+        }
+
+        public async Task AdicionarAsync(ContaPagar contaPagar)
+        {
+            
+        }
+
+        public async Task<List<ContasPagarResultViewModel>> BuscarContasPagarAsync(int mes, int ano, int? cartao)
+        {
+            var query = _contasPagar
+                .Where(p => p.DataVencimento.Month == mes && p.DataVencimento.Year == ano && p.FlCancelado == false).AsQueryable();
+
+            if (cartao is not null && cartao > 0)
+                query = query.Where(p => p.IdCartao == cartao);
+
+            return query.Select(p =>
+            new ContasPagarResultViewModel(
+                p.Descricao,
+                p.TotalParcela,
+                p.Valor,
+                p.IdCartao,
+                p.DataVencimento,
+                p.DataLancamento
+                )).ToList();
+        }
+    }
+}
